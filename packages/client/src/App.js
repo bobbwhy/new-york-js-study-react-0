e65b421297a4fa04a286712c144f5fc389477644
Robert LaMarca <robertlamarca@gmail.com>
2020-09-30 20:42:38 -0400

    hold here for a minute

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,9 +2,11 @@
 import React from "react";
 
 import { Header } from './components/header/header.component';
+import { MeetingsList } from './components/meetings/meetings-list/meetings-list.component';
 
 export default () => (
   <>
     <Header name="Chuck"/>
+    <MeetingsList />
   </>
 );
---
+++ packages/client/src/components/meetings/meetings-list/meetings-list.component.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+
+// import { fakeMeetings } from '../../../../../fake/meetings.log';
+
+
+import { HeaderBlank } from '../../header/header-blank.component';
+
+
+
+export const MeetingsList = props => {
+
+  return (
+    <>
+      <HeaderBlank />
+      <h2>Meetings List</h2>
+      {/*<div>{ JSON.stringify( fakeMeetings ) }</div>*/}
+  </>
+  )
+}
